01c664fd8db01ce69b11514dd5be34a4f4767715
ccadran <clariocadran33@gmail.com>
2024-03-29 16:14:31 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    "@": "/<srcDir>",
+  },
+  css: ["~/assets/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
